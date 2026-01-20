656c581e8c9d122101ea8e2823fb552cf3491a7c
SandalaNithin <sandalanithinkumar123@gmail.com>
2026-01-20 12:24:00 +0530

    PORT

--- server.js
+++ server.js
@@ -31,10 +31,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: "Something went wrong!" });
 });
 
-// // Start Server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+// Start Server
+const PORT = process.env.PORT || 5000;
 
-console.log('https://hall-1-6ub7.onrender.com');
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
