695096ed4f4d70fba3e58859061b62c09e0ffea3
SandalaNithin <sandalanithinkumar123@gmail.com>
2026-01-23 21:45:24 +0530

    baseurl

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hall-1-6ub7.onrender.com",
+  baseURL: "https://hall-3-0.onrender.com",
+  timeout: 30000, // 30 seconds timeout for slow operations like email sending
+  headers: {
+    "Content-Type": "application/json"
+  },
+  withCredentials: true
 });
 
 // Public booking APIs
